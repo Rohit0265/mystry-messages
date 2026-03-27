a125c139bdc5798188fe0de75cacd2ac73897fed
Rohit0265 <rohitmathur00981@gmail.com>
2026-03-27 23:47:41 +0530

    done today

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+'use client'
+
+import { Message } from '@/model/User'
+import React, { useState } from 'react'
+import { toast } from 'sonner'
 
 const Dashboard = () => {
+  const [messages,setMessages] = useState<Message[]>([])
+  const [isLoading,setIsLoading] = useState(false)
+  const [isSwitchLoading,setIsSwitchLoading] = useState(false)
+  // toast()
+  const handleDeleteMessage = (messageId:string)=>{
+    setMessages(messages.filter((message)=>message._id !== messageId))
+  }
   return (
     <div>Dashboard</div>
   )
