dbd06fd4336ff0410790c3ad8b4aafce3cc5c12c
Rohit0265 <rohitmathur00981@gmail.com>
2026-03-27 22:42:04 +0530

    sorry

--- src/components/messageCard.tsx
+++ src/components/messageCard.tsx
@@ -1,42 +1,98 @@
+
+
 'use client'
 
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
+
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Message } from "@/model/User"
+import { toast } from "sonner"
+import axios from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
 
-const MessageCard = () => {
-  return (
- <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
-      </CardHeader>
+type MessageCardProps = {
+    message:Message;
+    onMessageDelete:(messageId:string)=>void
 
-      <CardContent>
+}
 
 
-      </CardContent>
-      
-      <CardFooter className="flex-col gap-2">
 
-      </CardFooter>
-    </Card>
-  )
+
+
+const MessageCard = ({message,onMessageDelete}:MessageCardProps) => {
+
+        const handleDeleteConfirm = async ()=>{
+            const response =await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+            toast(response.data.message)
+            onMessageDelete(message._id)
+        }
+
+
+
+
+    return (
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>Login to your account</CardTitle>
+                <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                        <Button className="w-5 h-5" variant="destructive">Show Dialog</Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will permanently delete your
+                                account from our servers.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+                <CardDescription>
+                    Enter your email below to login to your account
+                </CardDescription>
+                <CardAction>
+                    <Button variant="link">Sign Up</Button>
+                </CardAction>
+            </CardHeader>
+
+            <CardContent>
+
+
+            </CardContent>
+
+            <CardFooter className="flex-col gap-2">
+
+            </CardFooter>
+        </Card>
+    )
 }
 
 export default MessageCard
